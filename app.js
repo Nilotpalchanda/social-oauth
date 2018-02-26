9d9b35d4ae93ad1ba00e68eca32c0555a1171ca4
Nilotpalchanda <nilotpalchanda14@gmail.com>
2018-02-27 01:14:32 +0530

    add profile for heroku

--- app.js
+++ app.js
@@ -48,7 +48,7 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
 app.listen(port)
-console.log(`server listen -> ${port}`)
+//console.log(`server listen -> ${port}`)
 
 app.get('/', (req, res) => {
     res.render('index',{user:req.user})
